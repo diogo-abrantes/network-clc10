4c1519d1f0b0ecc277d2ae3404beacba09ca02f5
EC2 Default User <ec2-user@ip-172-31-68-167.ec2.internal>
2023-10-08 04:07:49 +0000

    testar action

--- prod/main.tf
+++ prod/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
     default_tags {
         tags = {
             Owner   = "Diogo Abrantes"
-            Env     = "Producao teste"
+            Env     = "Producao"
             Class   = "CLC10"
         }
     }
